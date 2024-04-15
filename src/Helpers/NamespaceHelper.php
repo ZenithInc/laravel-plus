39229ff1b4f5c5a00086ddb54f4f043585943e91
ZenithSu <166780869+SuZenith@users.noreply.github.com>
2024-04-15 16:44:53 +0800

    Refactor path2namespace function in NamespaceHelper
    The path2namespace function in NamespaceHelper has been refactored for simplicity. The usage of constant DIRECTORY_SEPARATOR reduced the number of lines as well as making the file path compatible across different systems. In addition, using native array_filter function increases the overall performance. The code has been further simplified by straightly using '\\' for concatenating namespace parts, instead of considering different situations.

--- src/Helpers/NamespaceHelper.php
+++ src/Helpers/NamespaceHelper.php
@@ -14,16 +14,12 @@ class NamespaceHelper
      */
     public static function path2namespace(string $filePath): string
     {
-        $isWindows = strtoupper(substr(PHP_OS, 0, 3)) === 'WIN';
-        $isWindows && $filePath = str_replace('/', '\\', $filePath);
+        $filePath = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $filePath);
         $explodedPath = explode(DIRECTORY_SEPARATOR, $filePath);
-        $explodedPath = collect($explodedPath)->filter(fn ($path) => ! empty($path))->toArray();
+        $explodedPath = array_filter($explodedPath, fn ($path) => ! empty($path));
         $indexOfApp = array_search('app', $explodedPath);
         $namespaceArray = array_slice($explodedPath, $indexOfApp);
-        $namespace = implode(DIRECTORY_SEPARATOR, $namespaceArray);
-        $isWindows && str_replace('/', '', $namespace);
-        ! $isWindows && $namespace = 'App/'.$namespace;
-        ! $isWindows && $namespace = str_replace('/', '\\', $namespace);
+        $namespace = implode('\\', $namespaceArray);
         return ucfirst(str_replace('.php', '', $namespace));
     }
 }
