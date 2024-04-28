b4ff9b5feab8198499064efa9d469d5c2a3f3fc8
ZenithSu <166780869+SuZenith@users.noreply.github.com>
2024-04-28 15:07:27 +0800

    Add Event, Listener and Subscriber classes
    Three new empty classes under the `EventDriven` namespace, namely `Event`, `Listener`, and `Subscriber`, have been added. These classes have the `Attribute` target class annotation declaring them as custom attributes.

---
+++ src/Attributes/EventDriven/Event.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zenith\LaravelPlus\Attributes\EventDriven;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Event
+{
+
+}
---
+++ src/Attributes/EventDriven/Listener.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zenith\LaravelPlus\Attributes\EventDriven;
+
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS)]
+class Listener
+{
+
+}
+
