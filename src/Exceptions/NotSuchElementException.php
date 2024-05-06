c0bfaf227a190498930ee42a2cd59ee5a922c116
ZenithSu <happy@hakcing.icu>
2024-05-07 00:20:08 +0800

    supported Optional

---
+++ src/Exceptions/NotSuchElementException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zenith\LaravelPlus\Exceptions;
+
+use Exception;
+
+class NotSuchElementException extends Exception
+{
+
+}
---
+++ src/Optional.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zenith\LaravelPlus;
+
+use UnexpectedValueException;
+use Zenith\LaravelPlus\Exceptions\NotSuchElementException;
+
+class Optional
+{
+    private mixed $value;
+
+    public function __construct(mixed $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @throws NotSuchElementException
+     */
+    public function get(): mixed
+    {
+        if ($this->value === null) {
+            throw new NotSuchElementException();
+        }
+
+        return $this->value;
+    }
+
+    public static function of(mixed $value): self
+    {
+        if ($value === null) {
+            throw new UnexpectedValueException();
+        }
+
+        return new self($value);
+    }
+
+    public function ofNullable(mixed $value): self
+    {
+        $this->value = $value;
+
+        return new self($value);
+    }
+
+    public function ifPresent(callable $func): void
+    {
+        if ($this->value !== null) {
+            $func($this->value);
+        }
+    }
+
+    public function isPresent(): bool
+    {
+        return is_null($this->value);
+    }
+
+    public function ofElseThrow(callable|string $exception)
+    {
+        if ($this->value !== null) {
+            return $this->value;
+        }
+        if (is_string($exception)) {
+            throw new $exception();
+        }
+        throw new ($exception());
+    }
+}
