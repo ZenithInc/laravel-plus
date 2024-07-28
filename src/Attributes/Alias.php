7f6ae5773479898498b2088f7a01a2265bd958f8
SuZenith <happy@hakcing.icu>
2024-07-28 12:07:44 +0800

    Add support for class constants in Alias attribute
    Extended the Alias attribute to include class constants as valid targets. This enhances flexibility and allows for more precise usage of attributes across different code elements.

--- src/Attributes/Alias.php
+++ src/Attributes/Alias.php
@@ -6,7 +6,7 @@ namespace Zenith\LaravelPlus\Attributes;
 
 use Attribute;
 
-#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::TARGET_PROPERTY)]
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::TARGET_PROPERTY | Attribute::TARGET_CLASS_CONSTANT)]
 class Alias
 {
     public function __construct(
